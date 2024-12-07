8565ffec2c3e2215e94ec50691722823bc566159
emilrueh <emilrueh@googlemail.com>
2024-12-07 17:02:53 +0100

    Wrap in main

--- main.lua
+++ main.lua
@@ -6,18 +6,23 @@ local config = require("config")
 local call = utils.call
 local openai_api_key = config.openai_api_key
 
---
+-- TODO: make plan on when to call a first version done and post about it
+-- - loading prompts from file or cloud
+-- - basic logging instead of print
+-- - once streaming and cost tracking are implemented to be able to start löve gamedev
+-- - when published as lua rock
 
-local model = "gpt-4o-mini"
+local function main()
+	local model = "gpt-4o-mini"
 
-local system_prompt = "You respond only very briefly."
-local user_prompt = "What do you know about Bitburger?"
+	local system_prompt = "You respond only very briefly."
+	local user_prompt = "What do you know about Bitburger?"
 
-local reply = call(user_prompt, system_prompt, model, openai_api_key)
+	local reply, input_tokens, output_tokens = call(user_prompt, system_prompt, model, openai_api_key)
 
-if reply then
-	print(reply)
+	if reply then
+		print(reply)
+	end
 end
 
--- TODO: make plan on when to call a first version done and post about it
--- 		 e.g. once streaming and cost tracking are implemented to be able to start löve
+main()
