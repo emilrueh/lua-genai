18813a2c702b191361beec3c639b52fb8766025b
emilrueh <emilrueh@googlemail.com>
2024-12-08 19:44:20 +0100

    Build basic chatbot

--- main.lua
+++ main.lua
@@ -1,28 +1,30 @@
 -- require("lldebugger").start()
 
-local utils = require("utils")
-local config = require("config")
+local config = require("src.config")
+local utils = require("src.utils")
 
-local call = utils.call
 local openai_api_key = config.openai_api_key
+local call = utils.call
 
--- TODO: make plan on when to call a first version done and post about it
--- - loading prompts from file or cloud
--- - basic logging instead of print
--- - once streaming and cost tracking are implemented to be able to start löve gamedev
--- - when published as lua rock
-
-local function main()
+local function conversation()
 	local model = "gpt-4o-mini"
 
-	local system_prompt = "You respond only very briefly."
-	local user_prompt = "What do you know about Bitburger?"
+	local system_prompt = "Respond very briefly."
 
-	local reply, input_tokens, output_tokens = call(user_prompt, system_prompt, model, openai_api_key)
+	while true do
+		local user_prompt = io.read()
 
-	if reply then
+		if user_prompt == ":q" then
+			break
+		end
+
+		local reply, input_tokens, output_tokens = call(user_prompt, system_prompt, model, openai_api_key)
 		print(reply)
 	end
+end
+
+local function main()
+	conversation()
 end
 
 main()
