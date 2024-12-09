77e279fc8f474dc50a13b1c2464ae2dc089c92a9
emilrueh <emilrueh@googlemail.com>
2024-12-09 23:15:52 +0100

    Use Anthropic model

--- main.lua
+++ main.lua
@@ -3,11 +3,11 @@
 local config = require("src.config")
 local utils = require("src.utils")
 
-local openai_api_key = config.openai_api_key
+local anthropic_api_key = config.anthropic_api_key
 local call = utils.call
 
 local function conversation()
-	local model = "gpt-4o-mini"
+	local model = "claude-3-5-haiku-20241022"
 
 	local system_prompt = "Respond very briefly."
 
@@ -18,7 +18,7 @@ local function conversation()
 			break
 		end
 
-		local reply, input_tokens, output_tokens = call(user_prompt, system_prompt, model, openai_api_key)
+		local reply, input_tokens, output_tokens = call(user_prompt, system_prompt, model, anthropic_api_key)
 		print(reply)
 	end
 end
