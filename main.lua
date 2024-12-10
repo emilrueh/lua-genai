01b4f624870cee2eb1aca61cdc995232fb1f67c1
emilrueh <emilrueh@googlemail.com>
2024-12-10 22:57:02 +0100

    Refactor for message based input to allow chats with memory

--- main.lua
+++ main.lua
@@ -1,4 +1,10 @@
--- require("lldebugger").start()
+--
+
+if os.getenv("LOCAL_LUA_DEBUGGER_VSCODE") == "1" then
+	require("lldebugger").start()
+end
+
+--
 
 local config = require("src.config")
 local anthropic = require("src.ai.anthropic")
@@ -8,17 +14,29 @@ local anthropic_api_key = config.anthropic_api_key
 local function conversation()
 	local model = "claude-3-5-haiku-20241022"
 
-	local system_prompt = "Respond very briefly."
+	local system_prompt = "Respond extremely briefly and concise."
+
+	local messages = {}
+	table.insert(messages, { role = "system", content = system_prompt })
 
 	while true do
 		local user_prompt = io.read()
+		-- local user_prompt = "Hello, who are you?"
+		print()
 
 		if user_prompt == ":q" then
 			break
 		end
 
-		local reply, input_tokens, output_tokens = anthropic.call(user_prompt, system_prompt, model, anthropic_api_key)
+		table.insert(messages, { role = "user", content = user_prompt })
+
+		-- api call
+		local reply, input_tokens, output_tokens = anthropic.call(messages, model, anthropic_api_key)
+
+		table.insert(messages, { role = "assistant", content = reply })
+
 		print(reply)
+		print()
 	end
 end
 
--- src/ai/anthropic.lua
+++ src/ai/anthropic.lua
@@ -6,17 +6,42 @@ local json = config.json
 ---@module "src.ai.anthropic"
 local anthropic = {}
 
+---extract system prompt and confirm user prompt
+---@param messages table
+---@return string|nil system_prompt
+local function extract_and_confirm_messages(messages)
+	local system_prompt = nil
+	local user_prompt = nil
+
+	local copy = utils.shallow_copy_table(messages)
+
+	for i, message in ipairs(copy) do
+		-- extract user prompt to validate existance
+		if message.role == "user" then
+			user_prompt = message.content
+		-- extract system prompt and remove from messages
+		elseif message.role == "system" then
+			system_prompt = message.content
+			table.remove(messages, i)
+		end
+	end
+
+	assert(user_prompt, "A user prompt must be specified.")
+
+	return system_prompt
+end
+
 ---Anthropic API call to v1/messages endpoint
----@param user_prompt string
----@param system_prompt string|nil
+---@param messages table
 ---@param model string|nil
 ---@param api_key string
 ---@return string|nil reply
 ---@return integer|nil input_tokens
 ---@return integer|nil output_tokens
-function anthropic.call(user_prompt, system_prompt, model, api_key)
-	assert(user_prompt, "A user prompt must be specified.")
+function anthropic.call(messages, model, api_key)
+	local system_prompt = extract_and_confirm_messages(messages)
 
+	-- set defaults
 	model = model or "claude-3-5-haiku-20241022"
 
 	local endpoint = "https://api.anthropic.com/v1/messages"
@@ -26,9 +51,6 @@ function anthropic.call(user_prompt, system_prompt, model, api_key)
 		["anthropic-version"] = "2023-06-01", -- https://docs.anthropic.com/en/api/versioning
 		["content-type"] = "application/json",
 	}
-
-	local messages = {}
-	table.insert(messages, { role = "user", content = user_prompt })
 
 	local payload = json.encode({
 		model = model,
