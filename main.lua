28d855bdfd50380f887bc3ea3895d1efead9671d
emilrueh <emilrueh@googlemail.com>
2024-12-14 17:56:07 +0100

    Switch to OpenAI for accessibility

--- main.lua
+++ main.lua
@@ -4,13 +4,13 @@ local Chat = require("src.chat")
 
 local api_keys = config.api_keys
 
-local api_key = api_keys.anthropic_api_key
-local endpoint = "https://api.anthropic.com/v1/messages"
-local model = "claude-3-5-haiku-20241022"
+-- local api_key = api_keys.anthropic_api_key
+-- local endpoint = "https://api.anthropic.com/v1/messages"
+-- local model = "claude-3-5-haiku-20241022"
 
--- local api_key = api_keys.openai_api_key
--- local endpoint = "https://api.openai.com/v1/chat/completions"
--- local model = "gpt-4o-mini"
+local api_key = api_keys.openai_api_key
+local endpoint = "https://api.openai.com/v1/chat/completions"
+local model = "gpt-4o-mini"
 
 local system_prompt = "Respond extremely briefly."
 
