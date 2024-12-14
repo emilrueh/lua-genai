fcacdf89e8032272d42e96c4942aea5e0cdb02b6
emilrueh <emilrueh@googlemail.com>
2024-12-14 16:20:07 +0100

    Remove shallow copy

--- src/utils.lua
+++ src/utils.lua
@@ -28,15 +28,4 @@ function utils.send_request(url, data, method, headers)
 	return body
 end
 
----creates a shallow copy of a list like table
----@param t table
----@return table
-function utils.shallow_copy_table(t)
-	local copy = {}
-	for k, v in pairs(t) do
-		copy[k] = v
-	end
-	return copy
-end
-
 return utils
