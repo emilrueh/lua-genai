c40fd23a2808c74136a35096f8cb9a56681f8f03
emilrueh <emilrueh@googlemail.com>
2024-12-15 22:34:07 +0100

    Refactor call logic for stream option

--- src/ai.lua
+++ src/ai.lua
@@ -5,7 +5,7 @@ local providers = require("src.providers._load")
 
 local cjson = config.cjson
 
----AI API client for interacting with specified endpoint
+---Client for interacting with specified API endpoint
 ---@class AI
 ---@field _api_key string
 ---@field _endpoint string
@@ -28,7 +28,7 @@ function AI.new(api_key, endpoint)
 end
 
 ---Check endpoint for occurance of ai provider name
----@param providers table
+---@param providers table Collection of AI provider modules
 ---@return table|nil provider_module
 function AI:_determine_provider(providers)
 	for provider_name, provider_module in pairs(providers) do
@@ -41,15 +41,28 @@ function AI:_determine_provider(providers)
 end
 
 ---OpenAI API call to specified model
----@param opts table
----@return string reply
+---@param opts table Payload including model settings and chat history
+---@return string|nil reply
 function AI:call(opts)
 	local headers = self.provider.construct_headers(self._api_key)
 	local payload = cjson.encode(self.provider.construct_payload(opts))
-	local response = cjson.decode(utils.send_request(self._endpoint, payload, "POST", headers))
-	local reply, input_tokens, output_tokens = self.provider.extract_response_data(response)
 
-	return reply
+	-- TODO: make this file independent of specific setting structure like opts.settings.stream
+	-- - right now it Chat contains the stream = True setting
+	-- - perhaps it should be set within the AI client?
+
+	local callback = nil
+	if opts.settings.stream then
+		callback = self.provider.callback
+	end
+
+	local response = utils.send_request(self._endpoint, payload, "POST", headers, callback)
+
+	if callback then
+		return self.provider.assemble_stream_data()
+	else
+		return self.provider.extract_response_data(cjson.decode(response))
+	end
 end
 
 return AI
