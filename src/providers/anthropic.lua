5c719da2c69e469196117977f3d889dcc6317ba4
emilrueh <emilrueh@googlemail.com>
2024-12-15 22:30:50 +0100

    Implement settings and chunked data stream processing

--- src/providers/anthropic.lua
+++ src/providers/anthropic.lua
@@ -28,6 +28,15 @@ function anthropic.construct_assistant_message(reply)
 	return assistant_message
 end
 
+function anthropic.init_settings(settings)
+	settings = {
+		max_tokens = settings.max_tokens or 1024,
+		stream = settings.stream or false,
+	}
+
+	return settings
+end
+
 ---Construct the request headers
 ---@param api_key string
 ---@return table headers
@@ -45,16 +54,12 @@ end
 ---@param opts table
 ---@return table
 function anthropic.construct_payload(opts)
-	local messages = opts.history
-	local system_prompt = opts.system_prompt
-	local model = opts.model
-	local max_tokens = opts.max_tokens
-
 	local payload = {
-		model = model,
-		system = system_prompt,
-		messages = messages,
-		max_tokens = max_tokens or 1024, -- required
+		model = opts.model,
+		system = opts.system_prompt,
+		messages = opts.history,
+		max_tokens = opts.settings.max_tokens,
+		stream = opts.settings.stream,
 	}
 
 	return payload
@@ -69,6 +74,41 @@ function anthropic.extract_response_data(response)
 	local reply = response.content[1].text
 	local input_tokens = response.usage.input_tokens
 	local output_tokens = response.usage.output_tokens
+
+	return reply, input_tokens, output_tokens
+end
+
+---Parse and process Anthropic specific chunked responses structure
+---@param obj table JSON from string chunk
+---@param accumulator table Schema to collect chunked data
+local function handle_stream_data(obj, accumulator)
+	-- TODO: extract input and output tokens
+
+	if obj.type == "content_block_delta" and obj.delta and obj.delta.text then
+		local text = obj.delta.text
+
+		-- print chunked response text onto the same line
+		io.write(text)
+		io.flush()
+
+		-- accumulate response text
+		table.insert(accumulator.text, text)
+	end
+end
+
+---@type function Anthropic specific streamed data parsing and processing logic
+anthropic.callback = utils.create_sse_callback("^data:%s*(.*)", handle_stream_data)
+
+---Collect chunked data accumulated by callback
+---@return string reply Full response text accumulated from chunked responses
+---@return number input_tokens
+---@return number output_tokens
+function anthropic.assemble_stream_data()
+	local reply = table.concat(utils.accumulator.text)
+	local input_tokens = utils.accumulator.input_tokens
+	local output_tokens = utils.accumulator.output_tokens
+
+	utils.accumulator = utils.init_accumulator() -- reset
 
 	return reply, input_tokens, output_tokens
 end
