0f62ff7e3e252553559a4b8dd911a67e9bb0695e
emilrueh <emilrueh@googlemail.com>
2024-12-18 01:00:53 +0100

    Rethink and centralize stream response handling and split up call prep

--- src/ai.lua
+++ src/ai.lua
@@ -1,8 +1,6 @@
 local config = require("src.config")
 local utils = require("src.utils")
-
 local providers = require("src.providers._load")
-
 local cjson = config.cjson
 
 ---Client for interacting with specified API endpoint
@@ -14,51 +12,65 @@ local cjson = config.cjson
 local AI = {}
 AI.__index = AI
 
+---@param api_key string
+---@param endpoint string
 function AI.new(api_key, endpoint)
 	local self = setmetatable({}, AI)
-
 	self._api_key = api_key
 	self._endpoint = endpoint
-
-	-- Determine AI provider module to load
 	self.provider = self:_determine_provider(providers)
-	assert(self.provider, "AI provider could not be determined from provided endpoint")
-
 	return self
 end
 
 ---Check endpoint for occurance of ai provider name
 ---@param providers table Collection of AI provider modules
----@return table|nil provider_module
+---@return table|nil provider_module Collection of functions determining input and output structure
 function AI:_determine_provider(providers)
+	local provider = nil
 	for provider_name, provider_module in pairs(providers) do
-		if self._endpoint:find(provider_name) then return provider_module end
+		if self._endpoint:find(provider_name) then provider = provider_module end
 	end
-
-	return nil -- default if no provider found
+	assert(provider, "AI provider could not be determined from provided endpoint")
+	return provider
 end
 
----OpenAI API call to specified model
----@param opts table Payload including model settings and chat history
----@return string|nil reply
-function AI:call(opts)
-	local headers = self.provider.construct_headers(self._api_key)
-	local payload = cjson.encode(self.provider.construct_payload(opts))
-
-	-- TODO: make this file independent of specific setting structure like opts.settings.stream
-	-- - right now it Chat contains the stream = True setting
-	-- - perhaps it should be set within the AI client?
-
+---Prepare streaming requirements if set to stream
+---@param payload table
+---@return table|nil accumulator Schema storing full streamed response
+---@return function|nil callback Streaming handler
+function AI:_setup_stream(payload)
+	local accumulator = nil
 	local callback = nil
-	if opts.settings.stream then callback = self.provider.callback end
-
-	local response = utils.send_request(self._endpoint, payload, "POST", headers, callback)
-
-	if callback then
-		return self.provider.assemble_stream_data()
-	else
-		return self.provider.extract_response_data(cjson.decode(response))
+	if payload.stream then
+		accumulator = utils.Accumulator.new(cjson.encode(self.provider.response_schema))
+		local callback_opts = { self.provider.match_pattern, self.provider.handle_stream_data, accumulator }
+		callback = payload.stream and utils.create_sse_callback(callback_opts)
 	end
+	return accumulator, callback
+end
+
+---Prepare API call payload and streaming options
+---@param opts table Payload including model settings and chat history
+---@return table headers
+---@return table payload
+---@return function|nil callback Streaming handler
+---@return table|nil accumulator Schema storing full streamed response
+function AI:_prepare_response_requirements(opts)
+	local headers = self.provider.construct_headers(self._api_key)
+	local payload = self.provider.construct_payload(opts)
+	local accumulator, callback = self:_setup_stream(payload)
+	return headers, payload, callback, accumulator
+end
+
+---Execute API call to specified AI model with all payload and settings
+---@param opts table Payload including model settings and chat history
+---@return string reply
+---@return number input_tokens
+---@return number output_tokens
+function AI:call(opts)
+	local headers, payload, callback, accumulator = self:_prepare_response_requirements(opts)
+	local response = utils.send_request(self._endpoint, cjson.encode(payload), "POST", headers, callback)
+	return self.provider.extract_response_data(accumulator and accumulator.schema or cjson.decode(response))
 end
 
 return AI
