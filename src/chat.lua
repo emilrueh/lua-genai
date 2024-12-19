6f648791868cf58ee66ff4f723f6bf79511ff695
emilrueh <emilrueh@googlemail.com>
2024-12-19 21:14:10 +0100

    Track input and output tokens in usage

--- src/chat.lua
+++ src/chat.lua
@@ -1,9 +1,10 @@
----@class Chat
+---@class Chat Accumulating chat history and usage
 ---@field _ai table
----@field history table
----@field system_prompt string?
 ---@field model string
 ---@field settings table?
+---@field usage table
+---@field history table
+---@field system_prompt string?
 local Chat = {}
 Chat.__index = Chat
 
@@ -17,6 +18,7 @@ function Chat.new(ai, model, system_prompt, settings)
 	self._ai = ai
 	self.model = model
 	self.settings = self._ai.provider.init_settings(settings or {})
+	self.usage = { input = 0, output = 0 }
 	self.history = {}
 	self.system_prompt = system_prompt
 
@@ -34,8 +36,10 @@ end
 ---@return string reply Full response text whether streamed or not
 function Chat:say(user_prompt)
 	table.insert(self.history, self._ai.provider.construct_user_message(user_prompt))
-	local reply = self._ai:call(self)
+	local reply, input_tokens, output_tokens = self._ai:call(self)
 	table.insert(self.history, self._ai.provider.construct_assistant_message(reply))
+	self.usage.input = self.usage.input + input_tokens
+	self.usage.output = self.usage.output + output_tokens
 	return reply
 end
 
